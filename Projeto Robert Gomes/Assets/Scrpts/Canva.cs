17b021630f5d7bac218e0a9863127b8b7534f136
Osputo <100307357+Osputo@users.noreply.github.com>
2023-10-17 17:15:24 -0300

    40miloe

--- Projeto Robert Gomes/Assets/Scrpts/Canva.cs
+++ Projeto Robert Gomes/Assets/Scrpts/Canva.cs
@@ -9,18 +9,11 @@ public class Canva : MonoBehaviour
     public GameObject e;
     player player;
 
-    NPCTeste npc;
-    public string[] npcTeste;
-    public TMP_Text text;
-    int id = 0;
-
-    public GameObject Q;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType(typeof(player)) as player;
-        npc = FindObjectOfType<NPCTeste>();
     }
 
     // Update is called once per frame
