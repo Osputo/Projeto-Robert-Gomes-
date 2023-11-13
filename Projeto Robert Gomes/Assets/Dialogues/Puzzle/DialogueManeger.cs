4e6922f2843d38532a01142f4f5707db26772929
Osputo <tevaofornari@gmail.com>
2023-11-13 17:43:40 -0300

    pinto

--- Projeto Robert Gomes/Assets/Dialogues/Puzzle/DialogueManeger.cs
+++ Projeto Robert Gomes/Assets/Dialogues/Puzzle/DialogueManeger.cs
@@ -173,93 +173,14 @@ public class DialogueManeger : MonoBehaviour
         currentStory.ChooseChoiceIndex(choiceIndex);
     }
 
-
-
-
-    /*public TMP_Text nameText;
-    public TMP_Text dialogueText;
-    public float speedText;
-
-    
-
-    player player;
-
-    Queue<string> sentences;
-
-    // Start is called before the first frame update
-    void Start()
+    public Ink.Runtime.Object GetVariableState(string variableName)
     {
-        sentences = new Queue<string>();
-
-        player = FindObjectOfType(typeof(player)) as player;
-    }
-
-
-    private void Update()
-    {
-        switch (player.state)
+        Ink.Runtime.Object variableValue = null;
+        dialogueVariables.variables.TryGetValue(variableName, out variableValue);
+        if(variableName == null)
         {
-            case camState.dialogue:
-                if (Input.GetButtonDown("Fire2"))
-                    DisplayNextSetence();
+            Debug.LogWarning("Ink Variable was found to be null " + variableName);
 
-                break;
-        }                                  
-    }
-
-    public void StartDialogue(Dialogue dialogue)
-    {
-        animator.SetBool("IsOpen", true);
-
-        nameText.text = dialogue.name;
-
-        sentences.Clear();
-
-        player.state = camState.dialogue;
-
-        foreach(string sentence in dialogue.sentences)
-        {
-            sentences.Enqueue(sentence); 
         }
-
-        DisplayNextSetence();
+        return variableValue;
     }
-
-    public void DisplayNextSetence()
-    {
-        
-        
-            if (sentences.Count == 0)
-            {
-                EndDialogue();
-                return;
-            }
-
-            string setence = sentences.Dequeue();
-
-            StopAllCoroutines();
-
-            StartCoroutine(TypeSentence(setence));
-        
-        
-    }
-
-    IEnumerator TypeSentence(string sentence)
-    {
-        dialogueText.text = "";
-        foreach (char letter in sentence.ToCharArray())
-        {
-            dialogueText.text += letter;
-            yield return new WaitForSeconds(speedText);
-        }
-    }
-
-    void EndDialogue()
-    {
-        animator.SetBool("IsOpen", false);
-
-
-        player.state = camState.normal;
-    }*/
-
-}
